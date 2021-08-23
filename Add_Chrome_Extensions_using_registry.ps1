ae306cbb009973cc00f0b4e59cc589a440d67059
Dmitry Nefedov <nefedovds@ya.ru>
2021-08-23 21:01:27 +0300

    Update Add_Chrome_Extensions_using_registry.ps1

--- Add_Chrome_Extensions_using_registry.ps1
+++ Add_Chrome_Extensions_using_registry.ps1
@@ -2,11 +2,23 @@
 	.SYNOPSIS
 	Install Chrome Extensions using the registry
 
-	.PARAMETER ExtensionID
+	.PARAMETER ExtensionIDs
 	String value of an extension ID taken from the Chrome Web Store URL for the extension
 
+	.PARAMETER Path
+	A path where extensions will be stored
+
 	.EXAMPLE Install uBlock Origin and Tampermonkey
-	New-ChromeExtension -ExtensionID @("cjpalhdlnbpafiamejdnhcphjbkeiagm", "dhdgffkkebhmkfjojejmpbldmpobfkfo") -Verbose
+	$Parameters = @{
+		ExtensionID = @(
+			"cjpalhdlnbpafiamejdnhcphjbkeiagm",
+			"dhdgffkkebhmkfjojejmpbldmpobfkfo"
+		)
+		Path        = "C:\Users\test\Downloads\Extensions"
+		Verbose     = [switch]::Present
+	}
+	Add-ChromeExtension @Parameters
+
 
 	.NOTES
 	In order extensions work you need to store the .crx files
@@ -18,14 +30,17 @@ function Add-ChromeExtension
 	(
 		[Parameter(Mandatory = $true)]
 		[string[]]
-		$ExtensionIDs
+		$ExtensionIDs,
+
+		[Parameter(Mandatory = $true)]
+		[string]
+		$Path
 	)
 
 	# Create a folder to expand all files to
-	$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-	if (-not (Test-Path -Path "$DownloadsFolder\Extensions"))
+	if (-not (Test-Path -Path $Path))
 	{
-		New-Item -Path "$DownloadsFolder\Extensions" -ItemType Directory -Force
+		New-Item -Path $Path -ItemType Directory -Force
 	}
 
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -33,15 +48,15 @@ function Add-ChromeExtension
 	# Download 7ZIP 21.03 x64 due to .crx cannot be expanded: neither with System.IO.Compression.FileSystem, nor with the Expand-Archive cmdlet
 	$Parameters = @{
 		Uri = "https://www.7-zip.org/a/7z2103-x64.msi"
-		OutFile = "$DownloadsFolder\Extensions\7z2103-x64.msi"
+		OutFile = "$Path\7z2103-x64.msi"
 		Verbose = [switch]::Present
 	}
 	Invoke-WebRequest @Parameters
 
 	# Expand 7z2103-x64.msi to 7zip folder
 	$Arguments = @(
-		"/a `"$DownloadsFolder\Extensions\7z2103-x64.msi`""
-		"TARGETDIR=`"$DownloadsFolder\Extensions\7zip`""
+		"/a `"$Path\7z2103-x64.msi`""
+		"TARGETDIR=`"$Path\7zip`""
 		"/qb"
 	)
 	Start-Process "msiexec" -ArgumentList $Arguments -Wait
@@ -50,27 +65,27 @@ function Add-ChromeExtension
 	{
 		$Parameters = @{
 			Uri     = "https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromiumcrx&prodchannel=unknown&prodversion=52.0.2743.116&acceptformat=crx2,crx3&x=id%3D$($ExtensionID)%26uc"
-			OutFile = "$DownloadsFolder\Extensions\$ExtensionID.crx"
+			OutFile = "$Path\$ExtensionID.crx"
 			Verbose = [switch]::Present
 		}
 		Invoke-WebRequest @Parameters
 
 		# Copy file and rename it into .zip
-		Get-Item -Path "$DownloadsFolder\Extensions\$ExtensionID.crx" -Force | Foreach-Object -Process {
+		Get-Item -Path "$Path\$ExtensionID.crx" -Force | Foreach-Object -Process {
 			$NewName = $_.FullName -replace ".crx", ".zip"
 			Copy-Item -Path $_.FullName -Destination $NewName -Force
 		}
 
 		$Arguments = @(
 			"x",
-			"$DownloadsFolder\Extensions\$ExtensionID.crx",
-			"-o`"$DownloadsFolder\Extensions\$ExtensionID`"",
+			"$Path\$ExtensionID.crx",
+			"-o`"$Path\$ExtensionID`"",
 			"-y"
 		)
-		Start-Process "$DownloadsFolder\Extensions\7zip\Files\7-Zip\7z.exe" -ArgumentList $Arguments -Wait
+		Start-Process "$Path\7zip\Files\7-Zip\7z.exe" -ArgumentList $Arguments -Wait
 
 		# Get the version
-		$ExtensionVersion = (Get-Content -Path "$DownloadsFolder\Extensions\$ExtensionID\manifest.json" -Encoding Default -Force | ConvertFrom-Json).version
+		$ExtensionVersion = (Get-Content -Path "$Path\$ExtensionID\manifest.json" -Encoding Default -Force | ConvertFrom-Json).version
 
 		# https://www.chromium.org/administrators/pre-installed-extensions
 		# https://support.google.com/chrome/a/answer/187948
@@ -78,12 +93,20 @@ function Add-ChromeExtension
 		{
 			New-Item -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID -Force
 		}
-		New-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID -Name path -PropertyType String -Value "$DownloadsFolder\Extensions\$ExtensionID.crx" -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID -Name path -PropertyType String -Value "$Path\$ExtensionID.crx" -Force
 		New-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID -Name version -PropertyType String -Value $ExtensionVersion -Force
 
-		Remove-Item -Path "$DownloadsFolder\Extensions\$ExtensionID.zip", "$DownloadsFolder\Extensions\$ExtensionID" -Recurse -Force
+		Remove-Item -Path "$Path\$ExtensionID.zip", "$Path\$ExtensionID" -Recurse -Force
 	}
 
-	Remove-Item -Path "$DownloadsFolder\Extensions\7zip", "$DownloadsFolder\Extensions\7z2103-x64.msi" -Recurse -Force
+	Remove-Item -Path "$Path\7zip", "$Path\7z2103-x64.msi" -Recurse -Force
 }
-Add-ChromeExtension -ExtensionID @("cjpalhdlnbpafiamejdnhcphjbkeiagm", "dhdgffkkebhmkfjojejmpbldmpobfkfo") -Verbose
+$Parameters = @{
+	ExtensionID = @(
+		"cjpalhdlnbpafiamejdnhcphjbkeiagm",
+		"dhdgffkkebhmkfjojejmpbldmpobfkfo"
+	)
+	Path        = "C:\Users\test\Downloads\Extensions"
+	Verbose     = [switch]::Present
+}
+Add-ChromeExtension @Parameters
