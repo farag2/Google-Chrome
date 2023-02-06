ff675a6ec4bf9aa22c022582bf0393adfb543718
Dmitry Nefedov <nefedovds@ya.ru>
2023-02-07 00:14:52 +0300

    Update Add_Chrome_Extensions_using_registry.ps1

--- Add_Chrome_Extensions_using_registry.ps1
+++ Add_Chrome_Extensions_using_registry.ps1
@@ -14,11 +14,10 @@
 			"cjpalhdlnbpafiamejdnhcphjbkeiagm",
 			"dhdgffkkebhmkfjojejmpbldmpobfkfo"
 		)
-		Path        = "C:\Users\test\Downloads\Extensions"
-		Verbose     = [switch]::Present
+		Path    = "C:\Users\test\Downloads\Extensions"
+		Verbose = $true
 	}
 	Add-ChromeExtension @Parameters
-
 
 	.NOTES
 	In order extensions work you need to store the .crx files
@@ -45,17 +44,27 @@ function Add-ChromeExtension
 
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-	# Download 7ZIP 21.03 x64 due to .crx cannot be expanded: neither with System.IO.Compression.FileSystem, nor with the Expand-Archive cmdlet
+	# Get the latest 7-Zip download URL
 	$Parameters = @{
-		Uri = "https://www.7-zip.org/a/7z2103-x64.msi"
-		OutFile = "$Path\7z2103-x64.msi"
-		Verbose = [switch]::Present
+		Uri             = "https://sourceforge.net/projects/sevenzip/best_release.json"
+		UseBasicParsing = $true
+		Verbose         = $true
+	}
+	$bestRelease = (Invoke-RestMethod @Parameters).platform_releases.windows.filename.replace("exe", "msi")
+
+	# Download the latest 7-Zip x64
+	$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
+	$Parameters = @{
+		Uri             = "https://nchc.dl.sourceforge.net/project/sevenzip$($bestRelease)"
+		OutFile         = "$DownloadsFolder\7-Zip.msi"
+		UseBasicParsing = $true
+		Verbose         = $true
 	}
 	Invoke-WebRequest @Parameters
 
-	# Expand 7z2103-x64.msi to 7zip folder
+	# Expand 7-Zip
 	$Arguments = @(
-		"/a `"$Path\7z2103-x64.msi`""
+		"/a `"$Path\7-Zip.msi`""
 		"TARGETDIR=`"$Path\7zip`""
 		"/qb"
 	)
@@ -64,9 +73,9 @@ function Add-ChromeExtension
 	foreach ($ExtensionID in $ExtensionIDs)
 	{
 		$Parameters = @{
-			Uri     = "https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromiumcrx&prodchannel=unknown&prodversion=52.0.2743.116&acceptformat=crx2,crx3&x=id%3D$($ExtensionID)%26uc"
+			Uri     = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&acceptformat=crx3&x=id%3D$($ExtentionID)%26uc"
 			OutFile = "$Path\$ExtensionID.crx"
-			Verbose = [switch]::Present
+			Verbose = $true
 		}
 		Invoke-WebRequest @Parameters
 
@@ -99,7 +108,7 @@ function Add-ChromeExtension
 		Remove-Item -Path "$Path\$ExtensionID.zip", "$Path\$ExtensionID" -Recurse -Force
 	}
 
-	Remove-Item -Path "$Path\7zip", "$Path\7z2103-x64.msi" -Recurse -Force
+	Remove-Item -Path "$Path\7zip", "$Path\7-Zip.msi" -Recurse -Force
 }
 $Parameters = @{
 	ExtensionID = @(
@@ -107,6 +116,6 @@ $Parameters = @{
 		"dhdgffkkebhmkfjojejmpbldmpobfkfo"
 	)
 	Path        = "C:\Users\test\Downloads\Extensions"
-	Verbose     = [switch]::Present
+	Verbose     = $true
 }
 Add-ChromeExtension @Parameters
