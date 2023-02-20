dc38770962a50b2d5671b8659617d5e9e382ae15
Dmitry Nefedov <nefedovds@ya.ru>
2023-02-20 13:27:55 +0300

    Update Add_Chrome_Extensions_using_registry.ps1

--- Add_Chrome_Extensions_using_registry.ps1
+++ Add_Chrome_Extensions_using_registry.ps1
@@ -14,7 +14,7 @@
 			"cjpalhdlnbpafiamejdnhcphjbkeiagm",
 			"dhdgffkkebhmkfjojejmpbldmpobfkfo"
 		)
-		Path    = "C:\Users\test\Downloads\Extensions"
+		Path    = "D:\Downloads\Extensions"
 		Verbose = $true
 	}
 	Add-ChromeExtension @Parameters
@@ -36,44 +36,12 @@ function Add-ChromeExtension
 		$Path
 	)
 
-	# Create a folder to expand all files to
-	if (-not (Test-Path -Path $Path))
-	{
-		New-Item -Path $Path -ItemType Directory -Force
-	}
-
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-	# Get the latest 7-Zip download URL
-	$Parameters = @{
-		Uri             = "https://sourceforge.net/projects/sevenzip/best_release.json"
-		UseBasicParsing = $true
-		Verbose         = $true
-	}
-	$bestRelease = (Invoke-RestMethod @Parameters).platform_releases.windows.filename.replace("exe", "msi")
-
-	# Download the latest 7-Zip x64
-	$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-	$Parameters = @{
-		Uri             = "https://nchc.dl.sourceforge.net/project/sevenzip$($bestRelease)"
-		OutFile         = "$DownloadsFolder\7-Zip.msi"
-		UseBasicParsing = $true
-		Verbose         = $true
-	}
-	Invoke-WebRequest @Parameters
-
-	# Expand 7-Zip
-	$Arguments = @(
-		"/a `"$Path\7-Zip.msi`""
-		"TARGETDIR=`"$Path\7zip`""
-		"/qb"
-	)
-	Start-Process "msiexec" -ArgumentList $Arguments -Wait
 
 	foreach ($ExtensionID in $ExtensionIDs)
 	{
 		$Parameters = @{
-			Uri     = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&acceptformat=crx3&x=id%3D$($ExtentionID)%26uc"
+			Uri     = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&acceptformat=crx3&x=id%3D$($ExtensionID)%26uc"
 			OutFile = "$Path\$ExtensionID.crx"
 			Verbose = $true
 		}
@@ -85,13 +53,14 @@ function Add-ChromeExtension
 			Copy-Item -Path $_.FullName -Destination $NewName -Force
 		}
 
-		$Arguments = @(
-			"x",
-			"$Path\$ExtensionID.crx",
-			"-o`"$Path\$ExtensionID`"",
-			"-y"
-		)
-		Start-Process "$Path\7zip\Files\7-Zip\7z.exe" -ArgumentList $Arguments -Wait
+		# Create a folder to expand all files to
+		if (-not (Test-Path -Path "$Path\$ExtensionID"))
+		{
+			New-Item -Path "$Path\$ExtensionID" -ItemType Directory -Force
+		}
+
+		# Expand extension
+		& tar.exe -x -f "$Path\$ExtensionID.crx" -C "$Path\$ExtensionID" -v
 
 		# Get the version
 		$ExtensionVersion = (Get-Content -Path "$Path\$ExtensionID\manifest.json" -Encoding Default -Force | ConvertFrom-Json).version
@@ -107,15 +76,19 @@ function Add-ChromeExtension
 
 		Remove-Item -Path "$Path\$ExtensionID.zip", "$Path\$ExtensionID" -Recurse -Force
 	}
-
-	Remove-Item -Path "$Path\7zip", "$Path\7-Zip.msi" -Recurse -Force
 }
 $Parameters = @{
 	ExtensionID = @(
+		# https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm
 		"cjpalhdlnbpafiamejdnhcphjbkeiagm",
+		# https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo
 		"dhdgffkkebhmkfjojejmpbldmpobfkfo"
+		# https://chrome.google.com/webstore/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone
+		"mnjggcdmjocbbbhaepdhchncahnbgone"
+		# https://chrome.google.com/webstore/detail/return-youtube-dislike/gebbhagfogifgggkldgodflihgfeippi
+		"gebbhagfogifgggkldgodflihgfeippi"
 	)
-	Path        = "C:\Users\test\Downloads\Extensions"
-	Verbose     = $true
+	Path    = "D:\Downloads\Extensions"
+	Verbose = $true
 }
 Add-ChromeExtension @Parameters
