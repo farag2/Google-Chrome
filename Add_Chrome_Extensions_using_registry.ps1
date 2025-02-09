cbfe493a5656e31e1bda19f0126f14ebaabf386f
Dmitry Nefedov <nefedovds@ya.ru>
2025-02-09 15:00:21 +0300

    Update Add_Chrome_Extensions_using_registry.ps1

--- Add_Chrome_Extensions_using_registry.ps1
+++ Add_Chrome_Extensions_using_registry.ps1
@@ -4,23 +4,6 @@
 
 	.PARAMETER ExtensionIDs
 	String value of an extension ID taken from the Chrome Web Store URL for the extension
-
-	.PARAMETER Path
-	A path where extensions will be stored
-
-	.EXAMPLE Install uBlock Origin and Tampermonkey
-	$Parameters = @{
-		ExtensionID = @(
-			"cjpalhdlnbpafiamejdnhcphjbkeiagm",
-			"dhdgffkkebhmkfjojejmpbldmpobfkfo"
-		)
-		Path    = "D:\Downloads\Extensions"
-		Verbose = $true
-	}
-	Add-ChromeExtension @Parameters
-
-	.NOTES
-	In order extensions work you need to store the .crx files
 #>
 function Add-ChromeExtension
 {
@@ -29,66 +12,23 @@ function Add-ChromeExtension
 	(
 		[Parameter(Mandatory = $true)]
 		[string[]]
-		$ExtensionIDs,
-
-		[Parameter(Mandatory = $true)]
-		[string]
-		$Path
+		$ExtensionIDs
 	)
-
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 	foreach ($ExtensionID in $ExtensionIDs)
 	{
-		$Parameters = @{
-			Uri     = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&acceptformat=crx3&x=id%3D$($ExtensionID)%26uc"
-			OutFile = "$Path\$ExtensionID.crx"
-			Verbose = $true
-		}
-		Invoke-WebRequest @Parameters
-
-		# Copy file and rename it into .zip
-		Get-Item -Path "$Path\$ExtensionID.crx" -Force | Foreach-Object -Process {
-			$NewName = $_.FullName -replace ".crx", ".zip"
-			Copy-Item -Path $_.FullName -Destination $NewName -Force
-		}
-
-		# Create a folder to expand all files to
-		if (-not (Test-Path -Path "$Path\$ExtensionID"))
+		if (-not (Test-Path -Path "HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID"))
 		{
-			New-Item -Path "$Path\$ExtensionID" -ItemType Directory -Force
+			New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID" -Force
 		}
-
-		# Expand extension
-		& tar.exe -x -f "$Path\$ExtensionID.crx" -C "$Path\$ExtensionID" -v
-
-		# Get the version
-		$ExtensionVersion = (Get-Content -Path "$Path\$ExtensionID\manifest.json" -Encoding Default -Force | ConvertFrom-Json).version
-
-		# https://www.chromium.org/administrators/pre-installed-extensions
-		# https://support.google.com/chrome/a/answer/187948
-		if (-not (Test-Path -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID))
-		{
-			New-Item -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID -Force
-		}
-		New-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID -Name path -PropertyType String -Value "$Path\$ExtensionID.crx" -Force
-		New-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID -Name version -PropertyType String -Value $ExtensionVersion -Force
-
-		Remove-Item -Path "$Path\$ExtensionID.zip", "$Path\$ExtensionID" -Recurse -Force
+		New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Google\Chrome\Extensions\$ExtensionID" -Name update_url -PropertyType String -Value "https://clients2.google.com/service/update2/crx" -Force
 	}
 }
-$Parameters = @{
-	ExtensionID = @(
-		# https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm
-		"cjpalhdlnbpafiamejdnhcphjbkeiagm",
-		# https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo
-		"dhdgffkkebhmkfjojejmpbldmpobfkfo"
-		# https://chrome.google.com/webstore/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone
-		"mnjggcdmjocbbbhaepdhchncahnbgone"
-		# https://chrome.google.com/webstore/detail/return-youtube-dislike/gebbhagfogifgggkldgodflihgfeippi
-		"gebbhagfogifgggkldgodflihgfeippi"
-	)
-	Path    = "D:\Downloads\Extensions"
-	Verbose = $true
-}
-Add-ChromeExtension @Parameters
+Add-ChromeExtension -ExtensionIDs @(
+	# https://chromewebstore.google.com/detail/ublock-origin-lite/ddkjiahejlhfcafbddmgiahcphecmpfh
+	"ddkjiahejlhfcafbddmgiahcphecmpfh",
+	# https://chrome.google.com/webstore/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone
+ 	"mnjggcdmjocbbbhaepdhchncahnbgone"
+  	# https://chrome.google.com/webstore/detail/return-youtube-dislike/gebbhagfogifgggkldgodflihgfeippi
+   	"gebbhagfogifgggkldgodflihgfeippi"
+)
